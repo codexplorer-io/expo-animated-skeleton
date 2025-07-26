d77b0a2dea47a0a8cb16661518afa220d4b2e1bd
Marko Matic <mail.marko.matic@gmail.com>
2025-07-26 17:15:33 +1000

    - Fix animated skeleton for expo upgrade

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useRef } from 'react';
-import {
-    Animated,
-    StyleSheet,
-    Easing,
-    View
-} from 'react-native';
+import React, { useEffect, useMemo } from 'react';
 import { useTheme } from 'react-native-paper';
 import isArray from 'lodash/isArray';
 import forEach from 'lodash/forEach';
 import assign from 'lodash/assign';
-import { LinearGradient } from 'expo-linear-gradient';
-
-const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
+import {
+    Canvas,
+    Fill,
+    interpolateColors,
+    LinearGradient,
+    vec
+} from '@shopify/react-native-skia';
+import {
+    useDerivedValue,
+    useSharedValue,
+    withRepeat,
+    withTiming
+} from 'react-native-reanimated';
 
 export const AnimatedSkeleton = ({
     width,
@@ -19,26 +23,6 @@ export const AnimatedSkeleton = ({
     style
 }) => {
     const theme = useTheme();
-    const animated = useRef(new Animated.Value(0));
-
-    useEffect(() => {
-        Animated.loop(
-            Animated.timing(
-                animated.current,
-                {
-                    toValue: 1,
-                    duration: 1000,
-                    easing: Easing.linear.inOut,
-                    useNativeDriver: true
-                }
-            )
-        ).start();
-    }, []);
-
-    const translateX = animated.current.interpolate({
-        inputRange: [0, 1],
-        outputRange: [-width, width]
-    });
 
     const viewStyles = {};
     const extractViewStyles = ({
@@ -72,8 +56,36 @@ export const AnimatedSkeleton = ({
         extractViewStyles(style);
     }
 
+    const startColors = useMemo(() => [
+        theme.colors.skeletonLight,
+        theme.colors.skeletonDark,
+        theme.colors.skeletonDark,
+        theme.colors.skeletonLight
+    ], [theme.colors.skeletonLight, theme.colors.skeletonDark]);
+    const endColors = useMemo(() => [
+        theme.colors.skeletonDark,
+        theme.colors.skeletonLight,
+        theme.colors.skeletonLight,
+        theme.colors.skeletonDark
+    ], [theme.colors.skeletonLight, theme.colors.skeletonDark]);
+
+    const colorsIndex = useSharedValue(0);
+    useEffect(() => {
+        colorsIndex.value = withRepeat(
+            withTiming(startColors.length - 1, {
+                duration: 2000
+            }),
+            -1,
+            true
+        );
+    }, [colorsIndex, startColors.length]);
+    const gradientColors = useDerivedValue(() => [
+        interpolateColors(colorsIndex.value, [0, 1, 2, 3], startColors),
+        interpolateColors(colorsIndex.value, [0, 1, 2, 3], endColors)
+    ]);
+
     return (
-        <View
+        <Canvas
             // eslint-disable-next-line react/forbid-component-props
             style={[
                 viewStyles,
@@ -88,21 +100,18 @@ export const AnimatedSkeleton = ({
             width={width}
             height={height}
         >
-            <AnimatedLinearGradient
-                colors={[
-                    theme.colors.skeletonDark,
-                    theme.colors.skeletonLight,
-                    theme.colors.skeletonLight,
-                    theme.colors.skeletonDark
-                ]}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 1, y: 0 }}
-                // eslint-disable-next-line react/forbid-component-props
-                style={{
-                    ...StyleSheet.absoluteFill,
-                    transform: [{ translateX }]
-                }}
-            />
-        </View>
+            <Fill>
+                <LinearGradient
+                    colors={gradientColors ?? [
+                        theme.colors.skeletonDark,
+                        theme.colors.skeletonLight,
+                        theme.colors.skeletonLight,
+                        theme.colors.skeletonDark
+                    ]}
+                    start={vec(0, 0)}
+                    end={vec(width, 0)}
+                />
+            </Fill>
+        </Canvas>
     );
 };
